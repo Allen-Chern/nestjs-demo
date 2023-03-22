c8279d72c7cefd0a736e9162e42649b2bde8ce5d
love_u0302@hotmail.com <Allen Chen>
2023-03-22 21:54:03 +0800

    chore: use class-validator

--- core/user/dto/register.ts
+++ core/user/dto/register.ts
@@ -1,6 +1,15 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class RegisterDto {
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
   name: string;
+
+  @IsNotEmpty()
   password: string;
+
+  @IsNotEmpty()
   confirmPassword: string;
 }
--- core/user/dto/update-info.ts
+++ core/user/dto/update-info.ts
@@ -1,3 +1,6 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class UpdateInfoDto {
+  @IsNotEmpty()
   name: string;
 }
